52b472ab05b26783ab9aa2d0b8f35aae0e3f0225
Daniel Jeselnik <dannyjeselnik@gmail.com>
2018-12-11 16:36:16 -0500

    Added controller classes for new FXML files and corrected ConfirmEvent

---
+++ Time Management/src/edu/westga/cs3211/time_management/view/ConfirmEvent.java
@@ -0,0 +1,9 @@
+package edu.westga.cs3211.time_management.view;
+
+/** Codebehind for the ConfirmEvent Scene.
+ * 
+ * @author Jonathan Corley, Daniel Jeselnik
+ */
+public class ConfirmEvent {
+
+}
---
+++ Time Management/src/edu/westga/cs3211/time_management/view/UpdateEvent.java
@@ -0,0 +1,9 @@
+package edu.westga.cs3211.time_management.view;
+
+/** Codebehind for the UpdateEvent Scene.
+ * 
+ * @author Jonathan Corley, Daniel Jeselnik
+ */
+public class UpdateEvent {
+
+}
